22648e34acbf64e098937ed94b890b4afe3c80a0
Gus <springheeljim@gmail.com>
2014-01-06 18:57:32 +0000

    adds Publisher to Application and adds setUser call to assign a user to each client

--- Application.js
+++ Application.js
@@ -1,21 +1,23 @@
 define(function(require) {
+	var Publisher=require("lib/Publisher");
+	var List=require("lib/List");
+	
 	function Application(server) {
 		this._onlineUsersByUsername={};
 		this._tables=new List();
 		this._openChallenges=new List();
+		this._publisher=new Publisher();
 		
 		server.ClientConnected.addHandler(this, function(data) {
 			var client=data.client;
 			
-			//FIXME attach a user to the client somehow
+			if(client.getUser()===null) {
+				client.setUser(new User());
+			}
 			
-			client.MessageSent.addHandler(this, function() {
+			client.subscribe("/disconnect", (function() {
 				
-			});
-			
-			client.Disconnected.addHandler(this, function() {
-				
-			});
+			}).bind(this));
 		});
 	}
 	
