de950c3db34bfd81b0658eafeee0f449609ff271
Gus <springheeljim@gmail.com>
2014-01-06 18:58:23 +0000

    removes events from Client and gives it a Publisher instead.  adds getUser and setUser.

--- Client.js
+++ Client.js
@@ -1,34 +1,31 @@
 define(function(require) {
 	var Event=require("lib/Event");
 	var time=require("lib/time");
+	var Publisher=require("lib/Publisher");
 	
 	function Client(connection) {
 		this._connection=connection;
 		this._timeLastMessageReceived=null;
 		this._timeLastMessageSent=null;
 		this._timeConnected=time();
+		this._user=null;
+		this._publisher=new Publisher();
 		
-		this.SentMessage=new Event(this);
-		this.Disconnected=new Event(this);
-		
-		var self=this;
-		
-		this._connection.on("message", function(message) {
+		this._connection.on("message", (function(message) {
 			if(message.type==="utf8") {
-				self.SentMessage.fire({
-					message: JSON.parse(message.utf8Data)
-				});
-				
-				self._timeLastMessageSent=time();
+				this._publisher.publish(JSON.parse(message.utf8Data));
+				this._timeLastMessageSent=time();
 			}
-		});
+		}).bind(this));
 
-		this._connection.on("close", function(reason, description) {
-			self.Disconnected.fire({
-				reason: reason,
-				description: description
+		this._connection.on("close", (function(reason, description) {
+			this._publisher.publish({
+				"/disconnect": {
+					reason: reason,
+					description: description
+				}
 			});
-		});
+		}).bind(this));
 	}
 
 	Client.prototype.sendMessage=function(data) {
@@ -42,6 +39,14 @@ define(function(require) {
 	
 	Client.prototype.getTimeLastActive=function() {
 		return Math.max(this._timeConnected, this._timeLastMessageSent);
+	}
+	
+	Client.prototype.getUser=function() {
+		return this._user;
+	}
+	
+	Client.prototype.setUser=function(user) {
+		this._user=user;
 	}
 
 	return Client;
