98877b79f8507d1bf70f8388a167034fbec80b64
Gus <springheeljim@gmail.com>
2014-01-12 22:47:13 +0000

    adds username and debug message line to server User

--- User.js
+++ User.js
@@ -1,10 +1,12 @@
 define(function(require) {
 	var Publisher=require("lib/Publisher");
 	require("lib/Array.contains");
+	var id=require("lib/id");
 	
 	function User(client) {
 		this._client=client;
 		this._publisher=new Publisher();
+		this.username="Anonymous"+id();
 		
 		this._client.subscribe("*", (function(dataByUrl) {
 			this._publisher.publish(dataByUrl);
@@ -21,6 +23,7 @@ define(function(require) {
 	
 	User.prototype.send=function(dataByUrl) {
 		this._client.send(dataByUrl);
+		this._client.send(this.username);
 	}
 	
 	User.prototype._publish=function(dataByUrl) {
