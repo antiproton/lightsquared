c88f801e43515c2bd51c142573e5d5978a4d6d52
Gus <springheeljim@gmail.com>
2014-01-12 18:58:47 +0000

    adds message filter logic framework to User

--- User.js
+++ User.js
@@ -1,5 +1,6 @@
 define(function(require) {
 	var Publisher=require("lib/Publisher");
+	require("lib/Array.contains");
 	
 	function User(client) {
 		this._client=client;
@@ -12,8 +13,22 @@ define(function(require) {
 		this._ignoredUrls=[];
 		this._messageFiltersByUrl={};
 		
-		this._client.subscribe("/filter_add", (function(data) {
+		/*
+		TODO initialise filters with appropriate rating min/max etc
+		*/
+		
+		this._client.subscribe("/filter/add", (function(data) {
 			
+		}).bind(this));
+		
+		this._client.subscribe("/filter/remove", (function(data) {
+			//...
+			this._publish("/request/challenge_list");
+		}).bind(this));
+		
+		this._client.subscribe("/filter/clear", (function(url) {
+			delete this._messageFiltersByUrl[url];
+			this._publish("/request/challenge_list");
 		}).bind(this));
 	}
 	
@@ -26,7 +41,25 @@ define(function(require) {
 	}
 	
 	User.prototype.send=function(dataByUrl) {
-		this._client.send(dataByUrl);
+		var filteredData={};
+		
+		for(var url in dataByUrl) {
+			if(!this._ignoredUrls.contains(url)) {
+				if(url in this._messageFiltersByUrl) {
+					
+				}
+				
+				else {
+					filteredData[url]=dataByUrl[url];
+				}
+			}
+		}
+		
+		this._client.send(filteredData);
+	}
+	
+	User.prototype._publish=function(dataByUrl) {
+		this._publisher.publish(dataByUrl);
 	}
 	
 	return User;
