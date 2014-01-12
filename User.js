affd1b5c4a25f36e66f50c0dc5650426702fcf11
Gus <springheeljim@gmail.com>
2014-01-12 19:06:19 +0000

    removes filtering logic from User

--- User.js
+++ User.js
@@ -9,27 +9,6 @@ define(function(require) {
 		this._client.subscribe("*", (function(dataByUrl) {
 			this._publisher.publish(dataByUrl);
 		}).bind(this));
-		
-		this._ignoredUrls=[];
-		this._messageFiltersByUrl={};
-		
-		/*
-		TODO initialise filters with appropriate rating min/max etc
-		*/
-		
-		this._client.subscribe("/filter/add", (function(data) {
-			
-		}).bind(this));
-		
-		this._client.subscribe("/filter/remove", (function(data) {
-			//...
-			this._publish("/request/challenge_list");
-		}).bind(this));
-		
-		this._client.subscribe("/filter/clear", (function(url) {
-			delete this._messageFiltersByUrl[url];
-			this._publish("/request/challenge_list");
-		}).bind(this));
 	}
 	
 	User.prototype.subscribe=function(url, callback) {
@@ -41,21 +20,7 @@ define(function(require) {
 	}
 	
 	User.prototype.send=function(dataByUrl) {
-		var filteredData={};
-		
-		for(var url in dataByUrl) {
-			if(!this._ignoredUrls.contains(url)) {
-				if(url in this._messageFiltersByUrl) {
-					
-				}
-				
-				else {
-					filteredData[url]=dataByUrl[url];
-				}
-			}
-		}
-		
-		this._client.send(filteredData);
+		this._client.send(dataByUrl);
 	}
 	
 	User.prototype._publish=function(dataByUrl) {
