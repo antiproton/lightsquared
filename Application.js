7a793324aa2d074d033f8496985f0815bae97c34
Gus <springheeljim@gmail.com>
2014-01-12 00:45:36 +0000

    adds some stuff to do when a client connects to Application.  broadcast to other users that another user has connected, send the connecting user the count of users etc.  change lists from List to arrays; require Array.remove.  pass client to User constructor

--- Application.js
+++ Application.js
@@ -1,21 +1,47 @@
 define(function(require) {
 	var Publisher=require("lib/Publisher");
-	var List=require("lib/List");
+	require("lib/Array.remove");
 	var User=require("./User");
 	
 	function Application(server) {
-		this._onlineUsersByUsername={};
-		this._tables=new List();
-		this._openChallenges=new List();
+		this._server=server;
+		this._tables=[];
+		this._openChallenges=[];
 		this._publisher=new Publisher();
 		
 		server.ClientConnected.addHandler(this, function(data) {
 			var client=data.client;
 			
+			client.Disconnected.addHandler(this, function() {
+				this._sendBroadcastMessage({
+					"/user_disconnected": user.id
+				});
+			});
+			
 			if(!("user" in client.session)) {
-				client.session["user"]=new User();
+				client.session["user"]=new User(client);
 			}
+			
+			var user=client.session["user"];
+			
+			user.send({
+				"/users_online": this._countUsersOnline()
+			});
+			
+			/*
+			TODO send them full data of all the games they're in.
+			*/
+			
+			this._sendBroadcastMessage({
+				"/user_connected": user.id
+			});
 		});
+		
+		
+	}
+	
+	Application.prototype._sendBroadcastMessage=function(dataByUrl) {
+		this._server.sendBroadcastMessage(dataByUrl);
 	}
 	
 	return Application;
