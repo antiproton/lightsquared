53a38c65881d42e4d1c90da94976544a3aac93cc
Gus <springheeljim@gmail.com>
2014-01-12 00:48:36 +0000

    adds Publisher to User.  also adds client parameter to constructor.  adds ignoredUrls and _messageFiltersByUrl - system for determining whether a message is relevant to the user (e.g. no challenge list if they're not viewing the challenge list) and filtering it (e.g. min/max rating for challenges).

--- User.js
+++ User.js
@@ -1,6 +1,32 @@
 define(function(require) {
-	function User() {
+	var Publisher=require("lib/Publisher");
+	
+	function User(client) {
+		this._client=client;
+		this._publisher=new Publisher();
 		
+		this._client.subscribe("*", (function(dataByUrl) {
+			this._publisher.publish(dataByUrl);
+		}).bind(this));
+		
+		this.ignoredUrls=[];
+		this._messageFiltersByUrl={};
+		
+		this.subscribe("/filter_add", (function(data) {
+			
+		}).bind(this));
+	}
+	
+	User.prototype.subscribe=function(url, callback) {
+		this._client.subscribe(url, callback);
+	}
+	
+	User.prototype.unsubscribe=function(url, callback) {
+		this._client.unsubscribe(url, callback);
+	}
+	
+	User.prototype.send=function(dataByUrl) {
+		this._client.send(dataByUrl);
 	}
 	
 	return User;
