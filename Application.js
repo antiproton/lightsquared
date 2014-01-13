fc2ad7cc30b5cf2e320cc93bda738431bce667bd
Gus <springheeljim@gmail.com>
2014-01-13 22:52:10 +0000

    moves loading current tables into User and adds User.sendCurrentTables.  also adds logged in check and _isAnonymous flag to User

--- Application.js
+++ Application.js
@@ -23,18 +23,6 @@ define(function(require) {
 				"/challenges": this._openChallenges
 			});
 			
-			/*
-			TODO send them full data of all the games they're in.
-			
-			TODO go through games seeing which ones they're in, add them to their list
-			when they join/watch a game, add that one.  the above TODO can now be done
-			without looping through the games each time.
-			*/
-			
-			this._tables.forEach((function(table) {
-				
-			}).bind(this));
-			
 			this._sendBroadcastMessage({
 				"/user_connected": user.id
 			});
--- User.js
+++ User.js
@@ -6,7 +6,13 @@ define(function(require) {
 	function User(client) {
 		this._client=client;
 		this._publisher=new Publisher();
+		this._isAnonymous=true;
 		this.username="Anonymous"+id();
+		
+		if("username" in this._client.session) {
+			this.username=this._client.session["username"];
+			this._isAnonymous=false;
+		}
 		
 		this._client.Disconnected.addHandler(this, function() {
 			this._publisher.publish("/disconnected");
@@ -15,6 +21,22 @@ define(function(require) {
 		this._client.subscribe("*", (function(dataByUrl) {
 			this._publisher.publish(dataByUrl);
 		}).bind(this));
+	}
+	
+	User.prototype.sendCurrentTables=function(tables) {
+		if(!("current_tables" in this._client.session)) {
+			this._client.session["current_tables"]=[];
+			
+			tables.forEach((function(table) {
+				if(this.isAtTable(table)) {
+					this._client.session["current_tables"].push(table);
+				}
+			}).bind(this));
+		}
+		
+		this.send({
+			"/tables": this._client.session["current_tables"]
+		});
 	}
 	
 	User.prototype.subscribe=function(url, callback) {
@@ -27,6 +49,10 @@ define(function(require) {
 	
 	User.prototype.send=function(dataByUrl) {
 		this._client.send(dataByUrl);
+	}
+	
+	User.prototype.isAtTable=function(table) {
+		return (table.userIsSeated(this) || table.userIsWatching(this));
 	}
 	
 	return User;
