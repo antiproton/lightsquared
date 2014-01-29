f4556cc5cad3d6b2644857651555dd71129be17d
Gus <springheeljim@gmail.com>
2014-01-29 21:06:49 +0000

    adds some functionality to server Table

--- Table.js
+++ Table.js
@@ -1,6 +1,39 @@
 define(function(require) {
-	function Table() {
+	var Piece=require("chess/Piece");
+	var Game=require("./Game");
+	
+	function Table(owner) {
+		this._owner=owner;
 		
+		this._players=[];
+		this._players[Piece.WHITE]=null;
+		this._players[Piece.BLACK]=null;
+		
+		this._playerIsReady=[];
+		this._playerIsReady[Piece.WHITE]=false;
+		this._playerIsReady[Piece.BLACK]=false;
+		
+		this._game=null;
+	}
+	
+	Table.prototype.sit=function(user, colour) {
+		this._players[colour]=user;
+	}
+	
+	Table.prototype.ready=function(colour) {
+		this._playerIsReady[colour]=true;
+		
+		if(this._allPlayersAreReady) {
+			this._startGame();
+		}
+	}
+	
+	Table.prototype._startGame=function() {
+		this._game=new Game();
+	}
+	
+	Table.prototype._allPlayersAreReady=function() {
+		return (this._playerIsReady[Piece.WHITE] && this._playerIsReady[Piece.BLACK]);
 	}
 	
 	return Table;
