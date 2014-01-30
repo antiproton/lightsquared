291073c3a5e505848fa752c91d3f591d223de467
Gus <springheeljim@gmail.com>
2014-01-30 20:16:54 +0000

    adds some code to client and server Game classes

--- Game.js
+++ Game.js
@@ -1,16 +1,60 @@
 define(function(require) {
 	var ChessGame=require("chess/Game");
 	var Piece=require("chess/Piece");
+	var id=require("lib/id");
+	var Chess=require("chess/Chess");
 	
 	function Game(white, black) {
+		this._id=id();
 		this._game=new ChessGame();
 		
 		this._players=[];
 		this._players[Piece.WHITE]=white;
 		this._players[Piece.BLACK]=black;
+		
+		this._players.forEach((function(player) {
+			player.subscribe("/game/"+this._id+"/move", (function(data) {
+				var promoteTo=Piece.QUEEN;
+				
+				if(data.promoteTo!==undefined) {
+					promoteTo=data.promoteTo;
+				}
+				
+				this._move(player, data.from, data.to, promoteTo);
+			}).bind(this));
+			
+			player.subscribe("/game/"+this._id+"/resign", (function() {
+				this._resign(player);
+			}).bind(this));
+			
+			player.subscribe("/game/"+this._id+"/offer_draw", (function() {
+				this._offerDraw(player);
+			}).bind(this));
+		}).bind(this));
 	}
 	
+	Game.prototype.getId=function() {
+		return this._id;
+	}
 	
+	Game.prototype._move=function(user, from, to, promoteTo) {
+		var colour=this._game.getPosition().getActiveColor();
+		var oppColour=Chess.getOppColour(colour);
+		
+		if(this._players[colour]===user) {
+			var move=this._game.move(from, to, promoteTo);
+			
+			if(move.isLegal()) {
+				this._players[oppColour].send("/game/"+this._id+"/move", move);
+			}
+		}
+	}
+	
+	Game.prototype.toJSON=function() {
+		return {
+			history: this._game.getHistory()
+		};
+	}
 	
 	return Game;
 });
