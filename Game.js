e57747f8686a8c7e87bb98ecc8ba9966b151ab5f
Gus <springheeljim@gmail.com>
2014-01-31 02:07:03 +0000

    adds options, ratings and some toJSON code to server Game

--- Game.js
+++ Game.js
@@ -4,13 +4,36 @@ define(function(require) {
 	var id=require("lib/id");
 	var Chess=require("chess/Chess");
 	
-	function Game(white, black) {
+	function Game(white, black, options) {
 		this._id=id();
 		this._game=new ChessGame();
 		
 		this._players=[];
 		this._players[Piece.WHITE]=white;
 		this._players[Piece.BLACK]=black;
+		
+		this._oldRatings=[];
+		this._oldRatings[Piece.WHITE]=null;
+		this._oldRatings[Piece.BLACK]=null;
+		
+		this._newRatings=[];
+		this._newRatings[Piece.WHITE]=null;
+		this._newRatings[Piece.BLACK]=null;
+		
+		this._isUndoRequested=false;
+		this._isDrawOffered=false;
+		
+		this._options={
+			clockStartHalfmove: 1,
+			clockStartDelay: 0,
+			initialTime: 600,
+			increment: 0,
+			timingStyle: ChessGame.timingStyles.SUDDEN_DEATH,
+			isOvertime: false,
+			overtimeFullmove: 40,
+			overtimeBonus: 600,
+			isRated: true
+		};
 		
 		this._players.forEach((function(player) {
 			player.subscribe("/game/"+this._id+"/move", (function(data) {
@@ -54,7 +77,20 @@ define(function(require) {
 		return {
 			white: this._players[Piece.WHITE],
 			black: this._players[Piece.BLACK],
-			history: this._game.getHistory()
+			history: this._game.getHistory(),
+			state: this._game.getState(),
+			result: this._game.getResult(),
+			resultDetails: this._game.getResultDetails(),
+			startTime: this._game.getStartTime(),
+			endTime: this._game.getEndTime(),
+			isThreefoldClaimable: this._game.isThreefoldClaimable(),
+			isFiftymoveClaimable: this._game.isFiftymoveClaimable(),
+			whiteRatingOld: this._oldRatings[Piece.WHITE],
+			whiteRatingNew: this._newRatings[Piece.WHITE],
+			blackRatingOld: this._oldRatings[Piece.BLACK],
+			blackRatingNew: this._newRatings[Piece.BLACK],
+			isUndoRequested: this._isUndoRequested,
+			isDrawOffered: this._isDrawOffered
 		};
 	}
 	
