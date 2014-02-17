413fda59e11d2df3c24c1a02ec1a73b3396fadb6
Gus <springheeljim@gmail.com>
2014-02-17 00:22:35 +0000

    adds some login/logout logic to User

--- User.js
+++ User.js
@@ -9,6 +9,8 @@ define(function(require) {
 		this._user=user;
 		this._session=user.getSession();
 		this._username="Anonymous";
+		this._password=null;
+		this._isLoggedIn=false;
 		this._publisher=new Publisher();
 		this._gamesPlayedAsWhite=0;
 		this._gamesPlayedAsBlack=0;
@@ -18,6 +20,10 @@ define(function(require) {
 		
 		if("user" in this._session) {
 			this._loadJSON(this._session["user"]);
+		}
+		
+		else {
+			this._session["user"]=this.toJSON();
 		}
 		
 		this._user.Disconnected.addHandler(this, function() {
@@ -31,6 +37,18 @@ define(function(require) {
 		this._user.subscribe("*", (function(url, data) {
 			this._publisher.publish(url, data);
 		}).bind(this));
+		
+		this._user.subscribe("/user/login", function(data) {
+			this._login(data.username, data.password);
+		});
+		
+		this._user.subscribe("/user/logout", function() {
+			this._logout();
+		});
+		
+		this._user.subscribe("/user/register", function(data) {
+			this._register(data.username, data.password);
+		});
 	}
 	
 	User.prototype.getId=function() {
@@ -41,8 +59,32 @@ define(function(require) {
 		return this._id;
 	}
 	
-	User.prototype.login=function(username, password) {
+	User.prototype._login=function(username, password) {
+		if(!this._isLoggedIn) {
+			db.query("select * from users where username = ? and password = ?", username, password, function(rows) {
+				if(rows.length===1) {
+					this._loadJSON(rows[0]);
+					this._isLoggedIn=true;
+					this._user.send("/user/login/success");
+				}
+			});
+		}
 		
+		if(!this._isLoggedIn) {
+			this._user.send("/user/login/failure");
+		}
+	}
+	
+	User.prototype._logout=function() {
+		if(this._isLoggedIn) {
+			this._isLoggedIn=false;
+			this._username="Anonymous";
+			this._user.send("/user/logout");
+		}
+	}
+	
+	User.prototype._register=function(username, password) {
+		db.insert("users", this.toJSON()); //TODO
 	}
 	
 	User.prototype.sendCurrentTables=function(tables) {
@@ -83,6 +125,10 @@ define(function(require) {
 		return this._username;
 	}
 	
+	User.prototype.isLoggedIn=function() {
+		return this._isLoggedIn;
+	}
+	
 	User.prototype.getGamesAsWhiteRatio=function() {
 		Math.max(1, this._gamesPlayedAsWhite)/Math.max(1, this._gamesPlayedAsBlack);
 	}
@@ -95,10 +141,12 @@ define(function(require) {
 		};
 	}
 	
-	User.prototype._loadJSON=function(json) {
-		this._username=json.username;
-		this._gamesPlayedAsWhite=json.gamesPlayedAsWhite;
-		this._gamesPlayedAsBlack=json.gamesPlayedAsBlack;
+	User.prototype._loadJSON=function(data) {
+		this._username=data.username;
+		this._gamesPlayedAsWhite=data.gamesPlayedAsWhite;
+		this._gamesPlayedAsBlack=data.gamesPlayedAsBlack;
+		
+		this._publisher.publish("/update", this);
 	}
 	
 	return User;
