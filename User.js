67caae17b20b03fb0e20e6cc52247abe937f6563
Gus <springheeljim@gmail.com>
2014-02-19 20:01:25 +0000

    refactors User, changes session code, adds _save, fleshes out _register

--- User.js
+++ User.js
@@ -18,14 +18,6 @@ define(function(require) {
 		this.Connected=new Event(this);
 		this.Disconnected=new Event(this);
 		
-		if("user" in this._session) {
-			this._loadJSON(this._session["user"]);
-		}
-		
-		else {
-			this._session["user"]=this.toJSON();
-		}
-		
 		this._user.Disconnected.addHandler(this, function() {
 			this.Disconnected.fire();
 		});
@@ -34,6 +26,113 @@ define(function(require) {
 			this.Connected.fire();
 		});
 		
+		if(this._session.user) {
+			this._loadJSON(this._session.user);
+			this._isLoggedIn=this._session.isLoggedIn();
+		}
+		
+		this._session.user=this;
+		
+		this._subscribeToUserMessages();
+	}
+	
+	User.prototype.getId=function() {
+		return this._id;
+	}
+	
+	User.prototype.toString=function() {
+		return this._id;
+	}
+	
+	User.prototype._login=function(username, password) {
+		db.query("select * from users where username = ? and password = ?", [username, password], function(rows) {
+			if(rows.length===1) {
+				this._loadJSON(rows[0]);
+				this._isLoggedIn=true;
+				this._user.send("/user/login/success");
+			}
+			
+			else {
+				this._user.send("/user/login/failure");
+			}
+		});
+	}
+	
+	User.prototype._logout=function() {
+		if(this._isLoggedIn) {
+			this._isLoggedIn=false;
+			this._username="Anonymous";
+			this._user.send("/user/logout");
+		}
+	}
+	
+	User.prototype._register=function(username, password) {
+		db.query("select username from users where username = ?", [username], function(rows) {
+			if(rows.length===0) {
+				db.insert("users", this);
+				
+				this._user.send("/user/register/success", this);
+			}
+			
+			else {
+				this._user.send("/user/register/failure");
+			}
+		});
+	}
+	
+	User.prototype._save=function() {
+		db.update("users", this, {
+			username: this._username
+		});
+	}
+	
+	User.prototype.sendCurrentTables=function(tables) {
+		if(!(this._session.currentTables)) {
+			this._session.currentTables=[];
+			
+			var table;
+			
+			for(var id in tables) {
+				table=tables[id];
+				
+				if(this._isAtTable(table)) {
+					this._session.currentTables.push(table);
+				}
+			}
+		}
+		
+		this._user.send("/tables", this._session.currentTables);
+	}
+	
+	User.prototype.subscribe=function(url, callback) {
+		this._publisher.subscribe(url, callback);
+	}
+	
+	User.prototype.unsubscribe=function(url, callback) {
+		this._publisher.unsubscribe(url, callback);
+	}
+	
+	User.prototype.send=function(url, data) {
+		this._user.send(url, data);
+	}
+	
+	User.prototype._isAtTable=function(table) {
+		return (table.userIsSeated(this) || table.userIsWatching(this));
+	}
+	
+	User.prototype.getUsername=function() {
+		return this._username;
+	}
+	
+	User.prototype.isLoggedIn=function() {
+		return this._isLoggedIn;
+	}
+	
+	User.prototype.getGamesAsWhiteRatio=function() {
+		Math.max(1, this._gamesPlayedAsWhite)/Math.max(1, this._gamesPlayedAsBlack);
+	}
+	
+	User.prototype._subscribeToUserMessages=function() {
 		this._user.subscribe("*", (function(url, data) {
 			this._publisher.publish(url, data);
 		}).bind(this));
@@ -51,88 +150,6 @@ define(function(require) {
 		});
 	}
 	
-	User.prototype.getId=function() {
-		return this._id;
-	}
-	
-	User.prototype.toString=function() {
-		return this._id;
-	}
-	
-	User.prototype._login=function(username, password) {
-		if(!this._isLoggedIn) {
-			db.query("select * from users where username = ? and password = ?", username, password, function(rows) {
-				if(rows.length===1) {
-					this._loadJSON(rows[0]);
-					this._isLoggedIn=true;
-					this._user.send("/user/login/success");
-				}
-			});
-		}
-		
-		if(!this._isLoggedIn) {
-			this._user.send("/user/login/failure");
-		}
-	}
-	
-	User.prototype._logout=function() {
-		if(this._isLoggedIn) {
-			this._isLoggedIn=false;
-			this._username="Anonymous";
-			this._user.send("/user/logout");
-		}
-	}
-	
-	User.prototype._register=function(username, password) {
-		db.insert("users", this.toJSON()); //TODO
-	}
-	
-	User.prototype.sendCurrentTables=function(tables) {
-		if(!("current_tables" in this._session)) {
-			this._session["current_tables"]=[];
-			
-			var table;
-			
-			for(var id in tables) {
-				table=tables[id];
-				
-				if(this.isAtTable(table)) {
-					this._session["current_tables"].push(table);
-				}
-			}
-		}
-		
-		this.send("/tables", this._session["current_tables"]);
-	}
-	
-	User.prototype.subscribe=function(url, callback) {
-		this._publisher.subscribe(url, callback);
-	}
-	
-	User.prototype.unsubscribe=function(url, callback) {
-		this._publisher.unsubscribe(url, callback);
-	}
-	
-	User.prototype.send=function(url, data) {
-		this._user.send(url, data);
-	}
-	
-	User.prototype.isAtTable=function(table) {
-		return (table.userIsSeated(this) || table.userIsWatching(this));
-	}
-	
-	User.prototype.getUsername=function() {
-		return this._username;
-	}
-	
-	User.prototype.isLoggedIn=function() {
-		return this._isLoggedIn;
-	}
-	
-	User.prototype.getGamesAsWhiteRatio=function() {
-		Math.max(1, this._gamesPlayedAsWhite)/Math.max(1, this._gamesPlayedAsBlack);
-	}
-	
 	User.prototype.toJSON=function() {
 		return {
 			username: this._username,
@@ -142,11 +159,11 @@ define(function(require) {
 	}
 	
 	User.prototype._loadJSON=function(data) {
+		data=(data.toJSON instanceof Function?data.toJSON():data);
+		
 		this._username=data.username;
 		this._gamesPlayedAsWhite=data.gamesPlayedAsWhite;
 		this._gamesPlayedAsBlack=data.gamesPlayedAsBlack;
-		
-		this._publisher.publish("/update", this);
 	}
 	
 	return User;
