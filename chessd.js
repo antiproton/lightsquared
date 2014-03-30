2ed9c02eec798b49e3a0b9c63ee9911ad220ad05
Gus <springheeljim@gmail.com>
2014-03-30 04:04:41 +0100

    removes FIXME from chessd - issue has been posted to github

--- chessd.js
+++ chessd.js
@@ -3,8 +3,7 @@
 var requirejs = require("requirejs");
 var fs = require("fs");
 
-//FIXME this doesn't work; writes the wrong PID
-//fs.writeFileSync("/var/run/chessd.pid", process.pid.toString());
+fs.writeFileSync("/var/run/chessd.pid", process.pid.toString());
 
 requirejs.config({
 	nodeRequire: require,
