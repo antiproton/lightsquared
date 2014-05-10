b293304acc9da541c907f08e27205e1a86413e65
Gus <springheeljim@gmail.com>
2014-05-11 00:36:32 +0100

    removes scripts

--- scripts/install-lightsquared.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo ln -s /var/www/chess/scripts/lightsquared /etc/init.d/lightsquared
-sudo update-rc.d lightsquared defaults
--- scripts/uninstall-lightsquared.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo rm /etc/init.d/lightsquared
-sudo update-rc.d lightsquared remove
