192aa67be5bb65f27aa92de1dfb9466daabe66e7
Gus <gus@hogg-blake.co.uk>
2014-07-11 15:47:47 +0100

    changes Player to be an adapter between Users and the interface that Game etc use (just getRating), and to handle User replacement by having a setUser method.
    The Game doesn't have to handle Replaced because the Player stays the same, but
    the Player's reference to a User changes, as do the Users' internal Players -
    the new User gets the old User's Player, and the old User creates a new Player
    (this happens on logout).
    
    The /user/replaced functionality and the Replaced event are now gone; the user
    just logs out when they're replaced.

--- Player.js
+++ Player.js
@@ -1,42 +1,18 @@
 define(function(require) {
-	var Event = require("lib/Event");
-	
-	/*
-	player should remove games from its list as soon as they are over.
-	User can be responsible for persisting them for longer
-	*/
-	
-	function Player(app, rating) {
-		this._app = app;
-		
-		this._rating = rating;
-		this._currentGames = [];
-		this._currentChallenge = null;
-		this._lastChallengeOptions = null;
-		
-		
-		this.NewGame = new Event(this);
+	function Player(user) {
+		this._user = user;
 	}
 	
-	Player.prototype.setRating = function(rating) {
-		this._rating = rating;
+	Player.prototype.getRating = function() {
+		return this._user.getRating();
 	}
 	
-	/*
-	Player has a rating, but it's just set by whatever is using it.
-	
-	User sets it from the DB; Bot sets it based on the strength of the engine settings.
-	
-	It is currently only needed here because Game uses it to keep a record of
-	what the players' ratings were at the time.
-	*/
-	
-	Player.prototype.createChallenge = function(options) {
-		
+	Player.prototype.setUser = function(user) {
+		this._user = user;
 	}
 	
-	Player.prototype.acceptChallenge = function(id) {
-		
+	Player.prototype.toJSON = function() {
+		return this._user.toJSON();
 	}
 	
 	return Player;
